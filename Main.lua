18db1a4a1601ca74fdb07fdd87dd960f66f26e01
Pavi <pavi4@live.com>
2016-05-18 18:55:24 +0200

    Getting rid of Nasty red symbol by commiting

--- Main.lua
+++ Main.lua
@@ -37,7 +37,7 @@ bananatree = 3 -- random testing variable
 square = meter -- player size
 -----------------
 
-function shoot(bullet, dir) --used for deploying a bullet from the player in a specific direction 
+function shoot(bullet, dir) --used for deploying a bullet from the player in a specific direction
 
 	if _G["sniggle" .. dir] ==1 then    -- prevents autofire (_G[...]asks for variable in Global table)
 	local vel = bulletNormVel -- using right velocity
@@ -54,40 +54,40 @@ function shoot(bullet, dir) --used for deploying a bullet from the player in a s
     end
     end
     end
-	
+
 	_G["sniggle" .. dir] = _G["sniggle" .. dir]-1 -- preventing autofire part II
-		
+
 end
 
 function use(bullet, dir) --destroying the bullet, if...
-   
+
    for i, bullet in ipairs(objects.bullet[dir]) do
-    if bullet.isDone == 1 then -- ... its done and not used yet and... 
+    if bullet.isDone == 1 then -- ... its done and not used yet and...
 	bullet.body:setLinearVelocity(bullet.vel*_G["pos1" .. dir], bullet.vel*_G["pos2" .. dir]) -- (using the opportunity to make the bullet move)
 	local Velx, Vely = bullet.body:getLinearVelocity( ) -- getting our ocal variables to check
 	if Velx == 0 and _G["pos1" .. dir] ~= 0 then -- ... the bullet doesn't move in X direction if it's meant to
 	bullet.body:destroy()
-    bullet.isDone = 2 
+    bullet.isDone = 2
 	table.remove(objects.bullet[dir],i)
 	elseif Vely == 0 and _G["pos2" .. dir] ~= 0 then -- ... the bullet doesn't move in Y direction if it's meant to
 	bullet.body:destroy()
-    bullet.isDone = 2 
+    bullet.isDone = 2
 	table.remove(objects.bullet[dir],i)
 	elseif bullet.body:getX()-1 < 0 then -- ... if it's too far to the left
 	bullet.body:destroy()
-    bullet.isDone = 2 
+    bullet.isDone = 2
 	table.remove(objects.bullet[dir],i)
 	elseif bullet.body:getX()+1 > love.graphics.getWidth() then -- ... if it's too far to the right
 	bullet.body:destroy()
-    bullet.isDone = 2 
+    bullet.isDone = 2
 	table.remove(objects.bullet[dir],i)
 	elseif bullet.body:getY()-1 < 0 then -- ... if it's too far to the top
 	bullet.body:destroy()
-    bullet.isDone = 2 
+    bullet.isDone = 2
 	table.remove(objects.bullet[dir],i)
 	elseif bullet.body:getY()+floorheight+5 > love.graphics.getHeight() then -- ... if it's too far to the bottom
 	bullet.body:destroy()
-    bullet.isDone = 2 
+    bullet.isDone = 2
 	table.remove(objects.bullet[dir],i)
 	end
     end
@@ -95,7 +95,7 @@ function use(bullet, dir) --destroying the bullet, if...
 end
 
 function colldec (type1, dir1, type2, dir2, collA, collB) -- destroying two bodies if they touched
-    
+
     if dir1 == "" then -- when there is no direction, don't use it
 	for j, one in ipairs(objects[type1]) do -- opening our tables
     for i, two in ipairs(objects[type2][dir2]) do
@@ -115,7 +115,7 @@ function colldec (type1, dir1, type2, dir2, collA, collB) -- destroying two bodi
     end
     end
     end
-    
+
 	elseif dir2 == "" then -- when there is no direction, don't use it
 	for j, one in ipairs(objects[type1][dir1]) do -- opening our tables
     for i, two in ipairs(objects[type2]) do
@@ -135,7 +135,7 @@ function colldec (type1, dir1, type2, dir2, collA, collB) -- destroying two bodi
     end
     end
     end
-    
+
 	else
     for j, one in ipairs(objects[type1][dir1]) do  -- opening our tables
     for i, two in ipairs(objects[type2][dir2]) do
@@ -153,14 +153,14 @@ function colldec (type1, dir1, type2, dir2, collA, collB) -- destroying two bodi
 	    textvar = textvar+1
     end
     end
-    end   
+    end
     end
     end
 end
 
 function test(tree) -- testing stuff. To be removed.
 
-return _G["banana"..tree] 
+return _G["banana"..tree]
 
 end
 
@@ -168,21 +168,21 @@ function love.load(arg) -- loading stuff. Duh.
     math.randomseed(os.time()) -- makes random stuff more random
 	love.physics.setMeter(meter) --1 Meter = ...
     world = love.physics.newWorld(0, 9.81*10*meter, true) --no horizontal grav, 9.81m/s² vertically
-	
+
 	storage = {} -- some tables
-	
+
 	storage.collA = {}
-	
+
 	storage.collB = {}
-	
+
 	objects = {}
-	
+
 	objects.Ground = {}
-    objects.Ground.body = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight()-floorheight/2) --body, middle of shape will be at coordinate 
+    objects.Ground.body = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight()-floorheight/2) --body, middle of shape will be at coordinate
     objects.Ground.shape = love.physics.newRectangleShape(love.graphics.getWidth(), floorheight) --floor is ... high and all over the screen
     objects.Ground.fixture = love.physics.newFixture(objects.Ground.body, objects.Ground.shape) --attach shape to body
     objects.Ground.fixture:setUserData("Ground")
-	
+
 	objects.player = {}
     objects.player.body = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight()/2, "dynamic")
     objects.player.shape = love.physics.newRectangleShape( meter, meter)
@@ -191,7 +191,7 @@ function love.load(arg) -- loading stuff. Duh.
 	objects.player.speedX = 0
 	objects.player.speedY = 0
 	objects.player.fixture:setUserData("Playa")
-	
+
 	objects.bullet = {}
 	objects.bullet.R = {}
 	objects.bullet.DR = {}
@@ -201,50 +201,50 @@ function love.load(arg) -- loading stuff. Duh.
 	objects.bullet.UL = {}
 	objects.bullet.U = {}
 	objects.bullet.UR = {}
-	
+
 	objects.enemy = {}
-	
+
 	world:setCallbacks(beginContact, endContact, preSolve, postSolve) -- making collisison detection possible
 end
 
 function love.update(dt)
-    
+
 	   world:update(dt) -- gets the physics going
-	   
+
 	   objects.player.VelX, objects.player.VelY = objects.player.body:getLinearVelocityFromWorldPoint( 0, 0 ) -- putting player speed into variables
-	
+
 	if love.keyboard.isDown('escape') then -- makes you wanna quit
 	love.event.push('quit')
 	end
-	
+
 	if love.keyboard.isDown('a') then -- player controls
-	
+
 	if objects.player.VelX > -400 then
 	objects.player.body:applyLinearImpulse(-200,0)
 	end
-	
+
 	if love.keyboard.isDown('lshift') and objects.player.VelX > -800 then -- player controls
 	objects.player.body:applyLinearImpulse(-400,0)
 	end
-	
+
 	end
 
     if love.keyboard.isDown('d') then -- player controls
-	
+
 	if objects.player.VelX < 400 then
 	objects.player.body:applyLinearImpulse(200,0)
 	end
-	
+
 	if love.keyboard.isDown('lshift') and objects.player.VelX < 800 then -- player controls
 	objects.player.body:applyLinearImpulse(400,0)
 	end
-	
+
 	end
-	
+
     if love.keyboard.isDown(' ')  or love.keyboard.isDown('w') then -- player controls
-	
+
 	if objects.player.VelY < 10 and objects.player.VelY > -10 then
-	
+
 	if sniggleJ == 1 then -- no jumping autofire
 	sniggleJ = -1
 	objects.player.body:applyLinearImpulse(0,-1500)
@@ -252,26 +252,26 @@ function love.update(dt)
 	square = 40 -- makes the player go big after jumping
 	--------------
 	end
-	
+
 	end
-	
-	else	
+
+	else
 	sniggleJ = 1
 	end
-	
-	
+
+
 	if objects.player.body:getX() > love.graphics.getWidth() then -- don't leave the screen, please!
 	objects.player.body:setX(0)
 	end
-	
+
 	if objects.player.body:getX() < 0 then -- don't leave the screen, please!
 	objects.player.body:setX(love.graphics.getWidth())
 	end
-	
+
 	if objects.player.body:getY() < 0 then -- don't leave the screen, please!
 	objects.player.body:setLinearVelocity(0,100)
 	end
-	
+
 	if love.keyboard.isDown('kp6') then -- I'm firin' mah bullets
 	shoot(bullet, "R")
 	else sniggleR = 1
@@ -281,37 +281,37 @@ function love.update(dt)
 	shoot(bullet, "DR")
 	else sniggleDR = 1
 	end
-	
+
 	if love.keyboard.isDown('kp2') then -- I'm firin' mah bullets
 	shoot(bullet, "D")
 	else sniggleD = 1
 	end
-	
+
 	if love.keyboard.isDown('kp1') then -- I'm firin' mah bullets
 	shoot(bullet, "DL")
 	else sniggleDL = 1
 	end
-	
+
 	if love.keyboard.isDown('kp4') then -- I'm firin' mah bullets
 	shoot(bullet, "L")
 	else sniggleL = 1
 	end
-	
+
 	if love.keyboard.isDown('kp7') then -- I'm firin' mah bullets
 	shoot(bullet, "UL")
 	else sniggleUL = 1
 	end
-	
+
 	if love.keyboard.isDown('kp8') then -- I'm firin' mah bullets
 	shoot(bullet, "U")
 	else sniggleU = 1
 	end
-	
+
 	if love.keyboard.isDown('kp9') then -- I'm firin' mah bullets
 	shoot(bullet, "UR")
 	else sniggleUR = 1
 	end
-	
+
 	if math.random(0,6) == 3 and enemycount < maxEnemies then -- spawn enemies at random until there are enough
 	local velX = math.random(200,400)*(-1)^math.random(1,2) -- spawn enemies at random locations
 	local velY = math.random(200,400)*(-1)^math.random(1,2) -- spawn enemies at random locations
@@ -341,16 +341,16 @@ function love.update(dt)
    end
    end
    end
-   
+
 --------------------------------------------------JUMP EFFECT---------------------------------------
 	if square > 20 then -- funky jump rezising
 	   objects.player.shape = love.physics.newRectangleShape( square, square) --updated square shape
 	   square = square-0.5
-	else 
+	else
 	   objects.player.shape = love.physics.newRectangleShape( 20, 20) --ususal	square shape
 	   square = 20
 	end
-----------------------------------------------------------------------------------------------------	
+----------------------------------------------------------------------------------------------------
 for k, collA in ipairs(storage.collA) do -- calling our collision data
 for k, collB in ipairs(storage.collB) do
 colldec ("enemy", "", "bullet", "R", collA, collB) -- destroying everything that collided
@@ -393,7 +393,7 @@ love.graphics.setColor(255,255,255,255) -- make it all white
 	love.graphics.print(collB, k*70, 50)
 	end
 	love.graphics.print (test("tree"), 50, 50) -- testing stuff
-    
+
 
 for i, bullet in ipairs(objects.bullet.R) do -- drawing some bullets
     if bullet.isDone == 1 then
@@ -401,69 +401,69 @@ for i, bullet in ipairs(objects.bullet.R) do -- drawing some bullets
     love.graphics.print (bullet.fixture:getUserData(),100*i,90)
     end
 end
-	
+
 for i, bullet in ipairs(objects.bullet.DR) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,95)
     end
 end
-	
+
 for i, bullet in ipairs(objects.bullet.D) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,100)
     end
 end
-	
+
 for i, bullet in ipairs(objects.bullet.DL) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,105)
     end
 end
-	
+
 for i, bullet in ipairs(objects.bullet.L) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,110)
     end
 end
-	
+
 for i, bullet in ipairs(objects.bullet.UL) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,115)
 end
     end
-	
+
 for i, bullet in ipairs(objects.bullet.U) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,120)
     end
 end
-	
+
 for i, bullet in ipairs(objects.bullet.UR) do -- drawing some bullets
     if bullet.isDone == 1 then
 	love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     love.graphics.print (bullet.fixture:getUserData(),100*i,125)
     end
 end
-	
+
 love.graphics.setColor(255,0,0,255) -- some red for our enemies
-	
+
 for i, enemy in ipairs(objects.enemy) do  -- drawing some enemies
     if enemy.isDone == 1 then
     love.graphics.polygon("fill", enemy.body:getWorldPoints(enemy.shape:getPoints()))
 	love.graphics.print(tostring(enemy.velY),600, 10*i)
 	love.graphics.print(enemy.fixture:getUserData(),500, 10*i)
-    love.graphics.print(enemy.fixture:getUserData(),enemy.body:getX()+20, enemy.body:getY())	
+    love.graphics.print(enemy.fixture:getUserData(),enemy.body:getX()+20, enemy.body:getY())
     end
 end
-	
+
 end
-    
+
 function beginContact(a, b, coll)
 
 table.insert(storage.collA, a:getUserData()) --storing our collision data
@@ -472,18 +472,17 @@ table.insert(storage.collB, b:getUserData())
  if a:getUserData() == "Playa" and b:getUserData() == "Ground" or a:getUserData() == "Ground" and b:getUserData() == "Playa" then -- getting to know when the player touches the ground
  sniggleJ = 1
 end
- 
+
 end
-	
+
 function endContact(a, b, coll)
 
 end
-	
+
 function preSolve(a, b, coll)
 
-end	
-	
+end
+
 function postSolve(a, b, coll, normalimpulse1, tangentimpulse1, normalimpulse2, tangentimpulse2)
- 
-end	
-	
+
+end
