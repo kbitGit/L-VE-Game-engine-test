7528732e4fb75de7d136a5617c6dc8e69d8f585e
Pavi <pavi4@live.com>
2016-05-28 19:59:00 +0200

    - Unified Bullets object with dir attribute - fixed Enemy not dying bug(bullet still alive afterwards though) - some padding tweaking to make code a bit more readable

--- Main.lua
+++ Main.lua
@@ -37,127 +37,157 @@ bananatree = 3 -- random testing variable
 square = meter -- player size
 -----------------
 
-function shoot(bullet, dir) --used for deploying a bullet from the player in a specific direction
+function shoot(bullets, dir) --used for deploying a bullet from the player in a specific direction
 	if _G["sniggle" .. dir] ==1 then    -- prevents autofire (_G[...]asks for variable in Global table)
 		local vel = bulletNormVel -- using right velocity
 		local body = love.physics.newBody(world, objects.player.body:getX()+_G["pos1" .. dir], objects.player.body:getY()+_G["pos2" .. dir],'dynamic') -- creating the body at the right position
 		local shape = love.physics.newRectangleShape(4, 4) -- shaping up
-		table.insert(objects.bullet[dir], {body = body, shape = shape, vel = vel, isDone = 0}) -- putting the bullet in the right table
-		for i, bullet in ipairs(objects.bullet[dir]) do -- if the bullet isn't done yet, apply the final touches e.g. glueing everithing together and making it a bullet type object
-			if bullet.isDone == 0 then
-				bullet.fixture = love.physics.newFixture(bullet.body, bullet.shape, 0)
-				bullet.fixture:setUserData("bullet"..dir..i)
-				bullet.body:setBullet(true)
-				bullet.isDone = 1
+		table.insert(objects.bullets, {body = body, shape = shape, vel = vel, isDone = 0,dir=dir}) -- putting the bullet in the right table
+		for i, bullet in ipairs(objects.bullets) do -- if the bullet isn't done yet, apply the final touches e.g. glueing everithing together and making it a bullet type object
+			if bullet.dir == dir then
+				if bullet.isDone == 0 then
+					bullet.fixture = love.physics.newFixture(bullet.body, bullet.shape, 0)
+					bullet.fixture:setUserData("bullets"..i)
+					bullet.body:setBullet(true)
+					bullet.isDone = 1
+				end
 			end
 		end
 	end
 	_G["sniggle" .. dir] = _G["sniggle" .. dir]-1 -- preventing autofire part II
 end
 
-function use(bullet, dir) --destroying the bullet, if...
+function use(bullet) --destroying the bullet, if...
+  for i, bullet in ipairs(objects.bullets) do
+			if bullet.isDone == 1 then -- ... its done and not used yet and...
+				bullet.body:setLinearVelocity(bullet.vel*_G["pos1" .. bullet.dir], bullet.vel*_G["pos2" .. bullet.dir]) -- (using the opportunity to make the bullet move)
+				local Velx, Vely = bullet.body:getLinearVelocity( ) -- getting our ocal variables to check
+				if Velx == 0 and _G["pos1" .. bullet.dir] ~= 0 then -- ... the bullet doesn't move in X direction if it's meant to
+					bullet.body:destroy()
+  				bullet.isDone = 2
+					table.remove(objects.bullets,i)
+				elseif Vely == 0 and _G["pos2" .. bullet.dir] ~= 0 then -- ... the bullet doesn't move in Y direction if it's meant to
+					bullet.body:destroy()
+  				bullet.isDone = 2
+					table.remove(objects.bullets,i)
+				elseif bullet.body:getX()-1 < 0 then -- ... if it's too far to the left
+					bullet.body:destroy()
+  				bullet.isDone = 2
+					table.remove(objects.bullets,i)
+				elseif bullet.body:getX()+1 > love.graphics.getWidth() then -- ... if it's too far to the right
+					bullet.body:destroy()
+  				bullet.isDone = 2
+					table.remove(objects.bullets,i)
+				elseif bullet.body:getY()-1 < 0 then -- ... if it's too far to the top
+					bullet.body:destroy()
+  				bullet.isDone = 2
+					table.remove(objects.bullets,i)
+				elseif bullet.body:getY()+floorheight+5 > love.graphics.getHeight() then -- ... if it's too far to the bottom
+					bullet.body:destroy()
+  				bullet.isDone = 2
+					table.remove(objects.bullets,i)
+  		end
+		end
 
-   for i, bullet in ipairs(objects.bullet[dir]) do
-    if bullet.isDone == 1 then -- ... its done and not used yet and...
-	bullet.body:setLinearVelocity(bullet.vel*_G["pos1" .. dir], bullet.vel*_G["pos2" .. dir]) -- (using the opportunity to make the bullet move)
-	local Velx, Vely = bullet.body:getLinearVelocity( ) -- getting our ocal variables to check
-	if Velx == 0 and _G["pos1" .. dir] ~= 0 then -- ... the bullet doesn't move in X direction if it's meant to
-	bullet.body:destroy()
-    bullet.isDone = 2
-	table.remove(objects.bullet[dir],i)
-	elseif Vely == 0 and _G["pos2" .. dir] ~= 0 then -- ... the bullet doesn't move in Y direction if it's meant to
-	bullet.body:destroy()
-    bullet.isDone = 2
-	table.remove(objects.bullet[dir],i)
-	elseif bullet.body:getX()-1 < 0 then -- ... if it's too far to the left
-	bullet.body:destroy()
-    bullet.isDone = 2
-	table.remove(objects.bullet[dir],i)
-	elseif bullet.body:getX()+1 > love.graphics.getWidth() then -- ... if it's too far to the right
-	bullet.body:destroy()
-    bullet.isDone = 2
-	table.remove(objects.bullet[dir],i)
-	elseif bullet.body:getY()-1 < 0 then -- ... if it's too far to the top
-	bullet.body:destroy()
-    bullet.isDone = 2
-	table.remove(objects.bullet[dir],i)
-	elseif bullet.body:getY()+floorheight+5 > love.graphics.getHeight() then -- ... if it's too far to the bottom
-	bullet.body:destroy()
-    bullet.isDone = 2
-	table.remove(objects.bullet[dir],i)
-	end
-    end
 	end
 end
 
 function colldec (type1, dir1, type2, dir2, collA, collB) -- destroying two bodies if they touched
-
-    if dir1 == "" then -- when there is no direction, don't use it
-	for j, one in ipairs(objects[type1]) do -- opening our tables
-    for i, two in ipairs(objects[type2][dir2]) do
-    if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
-     if collA == (type1..i) and collB == (type2..dir2..j) or collA == (type2..dir2..j) and collB == (type1..i) then -- triggers when collision data matches object data
-        one.body:destroy()
-        one.isDone = 2
-	    table.remove(objects[type1],i)
-	    two.body:destroy()
-        two.isDone = 2
-	    table.remove(objects[type2][dir2],j)
-	    enemycount = enemycount-1
-	    table.remove(storage.collA,k)
-	    table.remove(storage.collB,k)
-	    textvar = textvar+1
-    end
-    end
-    end
-    end
-
-	elseif dir2 == "" then -- when there is no direction, don't use it
-	for j, one in ipairs(objects[type1][dir1]) do -- opening our tables
-    for i, two in ipairs(objects[type2]) do
-    if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
-     if collA == (type1..dir1..i) and collB == (type2..j) or collA == (type2..j) and collB == (type1..dir1..i) then -- triggers when collision data matches object data
-        one.body:destroy()
-        one.isDone = 2
-	    table.remove(objects[type1][dir1],i)
-	    two.body:destroy()
-        two.isDone = 2
-	    table.remove(objects[type2],j)
-	    enemycount = enemycount-1
-	    table.remove(storage.collA,k)
-	    table.remove(storage.collB,k)
-	    textvar = textvar+1
-    end
-    end
-    end
-    end
-
+	if dir1 == "" and dir2 =="" then -- when there is no direction, don't use it
+		for j, one in ipairs(objects[type1]) do -- opening our tables
+			for i, two in ipairs(objects[type2]) do
+				if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
+					if(collB == (type2..i)) then
+						print(collB)
+						print(type2..i)
+					end
+					if collA == (type1..j) and collB == (type2..i) or collA == (type2..i) and collB == (type1..j) then -- triggers when collision data matches object data
+						one.body:destroy()
+						one.isDone = 2
+						table.remove(objects[type1],i)
+						two.body:destroy()
+						two.isDone = 2
+						table.remove(objects[type2],j)
+						enemycount = enemycount-1
+						table.remove(storage.collA,k)
+						table.remove(storage.collB,k2)
+						textvar = textvar+1
+					end
+				end
+			end
+		end
+	elseif dir1~=""	and dir2 == "" then -- when there is no direction, don't use it
+		for j, one in ipairs(objects[type1][dir1]) do -- opening our tables
+  		for i, two in ipairs(objects[type2]) do
+  			if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
+   				if collA == (type1..dir1..i) and collB == (type2..j) or collA == (type2..j) and collB == (type1..dir1..i) then -- triggers when collision data matches object data
+  					one.body:destroy()
+  					one.isDone = 2
+  					table.remove(objects[type1][dir1],i)
+  					two.body:destroy()
+  					two.isDone = 2
+  					table.remove(objects[type2],j)
+  					enemycount = enemycount-1
+  					table.remove(storage.collA,k)
+  					table.remove(storage.collB,k)
+  					textvar = textvar+1
+  				end
+  			end
+  		end
+  	end
+	elseif dir1==""	and dir2 ~= "" then -- when there is no direction, don't use it
+		for j, one in ipairs(objects[type1]) do -- opening our tables
+			for i, two in ipairs(objects[type2][dir2]) do
+				if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
+					if collA == (type1..i) and collB == (type2..dir2..j) or collA == (type2..dir2..j) and collB == (type1..i) then -- triggers when collision data matches object data
+						one.body:destroy()
+						one.isDone = 2
+						table.remove(objects[type1],i)
+						two.body:destroy()
+						two.isDone = 2
+						table.remove(objects[type2][dir2],j)
+						enemycount = enemycount-1
+						table.remove(storage.collA,k)
+						table.remove(storage.collB,k)
+						textvar = textvar+1
+					end
+				end
+			end
+		end
 	else
-    for j, one in ipairs(objects[type1][dir1]) do  -- opening our tables
-    for i, two in ipairs(objects[type2][dir2]) do
-    if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
-     if collA == (type1..dir1..i) and collB == (type2..dir2..j) or collA == (type2..dir2..j) and collB == (type1..dir1..i) then -- triggers when collision data matches object data
-        one.body:destroy()
-        one.isDone = 2
-	    table.remove(objects[type1][dir1],i)
-	    two.body:destroy()
-        two.isDone = 2
-	    table.remove(objects[type2][dir2],j)
-	    enemycount = enemycount-1
-	    table.remove(storage.collA,k)
-	    table.remove(storage.collB,k)
-	    textvar = textvar+1
-    end
-    end
-    end
-    end
-    end
+		for j, one in ipairs(objects[type1][dir1]) do  -- opening our tables
+			for i, two in ipairs(objects[type2][dir2]) do
+				if one.isDone == 1 and two.isDone == 1 then -- only use the bodies if they are done and haven't been used yet
+					if collA == (type1..dir1..i) and collB == (type2..dir2..j) or collA == (type2..dir2..j) and collB == (type1..dir1..i) then -- triggers when collision data matches object data
+						one.body:destroy()
+						one.isDone = 2
+						table.remove(objects[type1][dir1],i)
+						two.body:destroy()
+						two.isDone = 2
+						table.remove(objects[type2][dir2],j)
+						enemycount = enemycount-1
+						table.remove(storage.collA,k)
+						table.remove(storage.collB,k)
+						textvar = textvar+1
+					end
+				end
+			end
+		end
+	end
 end
 
-function test(tree) -- testing stuff. To be removed.
-
-return _G["banana"..tree]
-
+function killEnemy(type1,collA)
+	for j, enemy in ipairs(objects[type1]) do  -- drawing some enemies
+			if enemy.isDone == 1  then
+				if collA == (type1..j) then
+					enemy.body:destroy()
+					enemy.isDone = 2
+					table.remove(enemy,j)
+					enemycount = enemycount-1
+					table.remove(storage.collA,k)
+				end
+			end
+		end
 end
 
 function love.load(arg) -- loading stuff. Duh.
@@ -182,15 +212,7 @@ function love.load(arg) -- loading stuff. Duh.
 	objects.player.speedY = 0
 	objects.player.fixture:setUserData("Playa")
 
-	objects.bullet = {}
-	objects.bullet.R = {}
-	objects.bullet.DR = {}
-	objects.bullet.D = {}
-	objects.bullet.DL = {}
-	objects.bullet.L = {}
-	objects.bullet.UL = {}
-	objects.bullet.U = {}
-	objects.bullet.UR = {}
+	objects.bullets = {}
 
 	objects.enemy = {}
 	while enemycount < maxEnemies do
@@ -241,7 +263,6 @@ function love.update(dt)
 		if love.keyboard.isDown('lshift') and objects.player.VelX < 800 then -- player controls
 			objects.player.body:applyLinearImpulse(400,0)
 		end
-
 	end
 
   if love.keyboard.isDown(' ')  or love.keyboard.isDown('w') then -- player controls
@@ -257,7 +278,6 @@ function love.update(dt)
 			end
 
 		end
-
 	else
 		sniggleJ = 1
 	end
@@ -276,42 +296,42 @@ function love.update(dt)
 	end
 
 	if love.keyboard.isDown('kp6') then -- I'm firin' mah bullets
-		shoot(bullet, "R")
+		shoot(bullets, "R")
 	else sniggleR = 1
 	end
 
 	if love.keyboard.isDown('kp3') then -- I'm firin' mah bullets
-		shoot(bullet, "DR")
+		shoot(bullets, "DR")
 	else sniggleDR = 1
 	end
 
 	if love.keyboard.isDown('kp2') then -- I'm firin' mah bullets
-		shoot(bullet, "D")
+		shoot(bullets, "D")
 	else sniggleD = 1
 	end
 
 	if love.keyboard.isDown('kp1') then -- I'm firin' mah bullets
-		shoot(bullet, "DL")
+		shoot(bullets, "DL")
 	else sniggleDL = 1
 	end
 
 	if love.keyboard.isDown('kp4') then -- I'm firin' mah bullets
-		shoot(bullet, "L")
+		shoot(bullets, "L")
 	else sniggleL = 1
 	end
 
 	if love.keyboard.isDown('kp7') then -- I'm firin' mah bullets
-		shoot(bullet, "UL")
+		shoot(bullets, "UL")
 	else sniggleUL = 1
 	end
 
 	if love.keyboard.isDown('kp8') then -- I'm firin' mah bullets
-		shoot(bullet, "U")
+		shoot(bullets, "U")
 	else sniggleU = 1
 	end
 
 	if love.keyboard.isDown('kp9') then -- I'm firin' mah bullets
-		shoot(bullet, "UR")
+		shoot(bullets, "UR")
 	else sniggleUR = 1
 	end
 
@@ -337,30 +357,12 @@ function love.update(dt)
 	end
 	----------------------------------------------------------------------------------------------------
 	for k, collA in ipairs(storage.collA) do -- calling our collision data
-		for k, collB in ipairs(storage.collB) do
-			colldec ("enemy", "", "bullet", "R", collA, collB) -- destroying everything that collided
-			colldec ("enemy", "", "bullet", "DR", collA, collB)
-			colldec ("enemy", "", "bullet", "D", collA, collB)
-			colldec ("enemy", "", "bullet", "DL", collA, collB)
-			colldec ("enemy", "", "bullet", "L", collA, collB)
-			colldec ("enemy", "", "bullet", "UL", collA, collB)
-			colldec ("enemy", "", "bullet", "U", collA, collB)
-			colldec ("enemy", "", "bullet", "UR", collA, collB)
-			if k > 10 then -- only store 10 collision files
-				table.remove(storage.collB,k-10)
-				table.remove(storage.collA,k-10)
-			end
+		killEnemy("enemy",collA)
+		if k > 10 then -- only store 10 collision files
+			table.remove(storage.collA,k-10)
 		end
 	end
-
-	use(bullet, "R") -- use every bullet
-	use(bullet, "DR")
-	use(bullet, "D")
-	use(bullet, "DL")
-	use(bullet, "L")
-	use(bullet, "UL")
-	use(bullet, "U")
-	use(bullet, "UR")
+	use(bullets) -- use every bullet
 end
 
 function love.draw(dt)
@@ -377,59 +379,10 @@ function love.draw(dt)
 
 	love.graphics.print (test("tree"), 50, 50) -- testing stuff
 
-	for i, bullet in ipairs(objects.bullet.R) do -- drawing some bullets
+	for i, bullet in ipairs(objects.bullets) do -- drawing some bullets
   	if bullet.isDone == 1 then
 			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
     	love.graphics.print (bullet.fixture:getUserData(),100*i,90)
-  	end
-	end
-
-	for i, bullet in ipairs(objects.bullet.DR) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-    	love.graphics.print (bullet.fixture:getUserData(),100*i,95)
-  	end
-	end
-
-	for i, bullet in ipairs(objects.bullet.D) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-    	love.graphics.print (bullet.fixture:getUserData(),100*i,100)
-  	end
-	end
-
-	for i, bullet in ipairs(objects.bullet.DL) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-    	love.graphics.print (bullet.fixture:getUserData(),100*i,105)
-  	end
-	end
-
-	for i, bullet in ipairs(objects.bullet.L) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-    	love.graphics.print (bullet.fixture:getUserData(),100*i,110)
-  	end
-	end
-
-	for i, bullet in ipairs(objects.bullet.UL) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-    	love.graphics.print (bullet.fixture:getUserData(),100*i,115)
-		end
-	end
-
-	for i, bullet in ipairs(objects.bullet.U) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-    	love.graphics.print (bullet.fixture:getUserData(),100*i,120)
-  	end
-	end
-
-	for i, bullet in ipairs(objects.bullet.UR) do -- drawing some bullets
-  	if bullet.isDone == 1 then
-			love.graphics.polygon("fill", bullet.body:getWorldPoints(bullet.shape:getPoints()))
-  		love.graphics.print (bullet.fixture:getUserData(),100*i,125)
   	end
 	end
 
@@ -443,15 +396,19 @@ function love.draw(dt)
   		love.graphics.print(enemy.fixture:getUserData(),enemy.body:getX()+20, enemy.body:getY())
   	end
 	end
-
 end
 
 function beginContact(a, b, coll)
-if a:getUserData() =="bullet" or b:getUserData()=="bullet" then
-		table.insert(storage.collA, a:getUserData()) --storing our collision data
-		table.insert(storage.collB, b:getUserData())
-end
+	if string.match(a:getUserData(),"bullet") and  string.match(b:getUserData(),"enemy") or string.match(a:getUserData(),"enemy") and  string.match(b:getUserData(),"bullet") then
 
+		if string.match(a:getUserData(),"enemy") then
+		table.insert(storage.collA, a:getUserData()) --storing our collision data
+		end
+		if string.match(b:getUserData(),"enemy")then
+		table.insert(storage.collA, b:getUserData())
+		end
+
+	end
  	if a:getUserData() == "Playa" and b:getUserData() == "Ground" or a:getUserData() == "Ground" and b:getUserData() == "Playa" then -- getting to know when the player touches the ground
  		sniggleJ = 1
 	end
