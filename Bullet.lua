54fdd148cc677db2d7cb2b73d0ac0e993ac637dd
TheLie0 <daniel.mowitz@online.de>
2016-07-26 17:30:36 +0200

    Update Bullet.lua

--- Bullet.lua
+++ Bullet.lua
@@ -1,59 +1,62 @@
 local M = {}
 
-function M.shoot(bullets, dir) --used for deploying a bullet from the player in a specific direction
-	if _G["sniggle" .. dir] ==1 then    -- prevents autofire (_G[...]asks for variable in Global table)
-		local vel = bulletNormVel -- using right velocity
-		local body = love.physics.newBody(world, objects.player.body:getX()+_G["pos1" .. dir], objects.player.body:getY()+_G["pos2" .. dir],'dynamic') -- creating the body at the right position
-		local shape = love.physics.newRectangleShape(4, 4) -- shaping up
-		table.insert(objects.bullets, {body = body, shape = shape, vel = vel, isDone = 0,dir=dir}) -- putting the bullet in the right table
-		for i, bullet in ipairs(objects.bullets) do -- if the bullet isn't done yet, apply the final touches e.g. glueing everithing together and making it a bullet type object
-			if bullet.dir == dir then
-				if bullet.isDone == 0 then
-					bullet.fixture = love.physics.newFixture(bullet.body, bullet.shape, 0)
-					bullet.fixture:setUserData("bullets"..i)
-					bullet.body:setBullet(true)
-					bullet.isDone = 1
+function M.killEnemy(type1,collA)
+	for j, enemy in ipairs(objects[type1]) do  -- drawing some enemies
+			if enemy.isDone == 1  then
+				if collA == (type1..j) then
+					enemy.body:destroy()
+					enemy.isDone = 2
+					table.remove(enemy,j)
+					enemycount = enemycount-1
+					table.remove(storage.collA,k)
 				end
 			end
 		end
-	end
-	_G["sniggle" .. dir] = _G["sniggle" .. dir]-1 -- preventing autofire part II
 end
 
-function M.use(bullet) --destroying the bullet, if...
-  for i, bullet in ipairs(objects.bullets) do
-			if bullet.isDone == 1 then -- ... its done and not used yet and...
-				bullet.body:setLinearVelocity(bullet.vel*_G["pos1" .. bullet.dir], bullet.vel*_G["pos2" .. bullet.dir]) -- (using the opportunity to make the bullet move)
-				local Velx, Vely = bullet.body:getLinearVelocity( ) -- getting our ocal variables to check
-				if Velx == 0 and _G["pos1" .. bullet.dir] ~= 0 then -- ... the bullet doesn't move in X direction if it's meant to
-					bullet.body:destroy()
-  				bullet.isDone = 2
-					table.remove(objects.bullets,i)
-				elseif Vely == 0 and _G["pos2" .. bullet.dir] ~= 0 then -- ... the bullet doesn't move in Y direction if it's meant to
-					bullet.body:destroy()
-  				bullet.isDone = 2
-					table.remove(objects.bullets,i)
-				elseif bullet.body:getX()-1 < 0 then -- ... if it's too far to the left
-					bullet.body:destroy()
-  				bullet.isDone = 2
-					table.remove(objects.bullets,i)
-				elseif bullet.body:getX()+1 > love.graphics.getWidth() then -- ... if it's too far to the right
-					bullet.body:destroy()
-  				bullet.isDone = 2
-					table.remove(objects.bullets,i)
-				elseif bullet.body:getY()-1 < 0 then -- ... if it's too far to the top
-					bullet.body:destroy()
-  				bullet.isDone = 2
-					table.remove(objects.bullets,i)
-				elseif bullet.body:getY()+floorheight+5 > love.graphics.getHeight() then -- ... if it's too far to the bottom
-					bullet.body:destroy()
-  				bullet.isDone = 2
-					table.remove(objects.bullets,i)
-  		end
+function M.SpawnEnemy(enemy)
+  while enemycount < maxEnemies do
+    local velX = math.random(200,400)*(-1)^math.random(1,2) -- spawn enemies at random locations
+    local velY = math.random(200,400)*(-1)^math.random(1,2) -- spawn enemies at random locations
+    local body = love.physics.newBody(world, math.random(0,love.graphics.getWidth()), math.random(0,love.graphics.getHeight()-50),'dynamic') -- give the enemies a body
+    local shape = love.physics.newRectangleShape(20, 20) -- shaping up
+    table.insert(objects.enemy, {body = body, shape = shape, velX = velX, velY = velY, isDone = 0}) -- put 'em into a table
+    enemycount = enemycount+1 -- count the enemies
+    for i, enemy in ipairs(objects.enemy) do -- add finishing touches
+      if enemy.isDone == 0 then
+        enemy.fixture = love.physics.newFixture(enemy.body, enemy.shape, 0)
+        enemy.fixture:setUserData("enemy"..i)
+        enemy.body:setBullet(true)
+        enemy.isDone = 1
+      end
+    end
+  end
+end
+
+function M.UpdateEnemy(enemy)
+  for i, enemy in ipairs(objects.enemy) do
+		if enemy.isDone == 1 then
+			enemy.body:setLinearVelocity(enemy.velX, enemy.velY) -- get the enemies moving
+			if enemy.body:getX()-5 < 0 and enemy.velX < 0 or enemy.body:getX()+5 > love.graphics.getWidth() and enemy.velX > 0 then -- reverse enemie speed if the move to the outside horizontally
+				enemy.velX = -enemy.velX
+			end
+			if enemy.body:getY()-5 < 0 and enemy.velY < 0 or enemy.body:getY()+5 > love.graphics.getHeight()-floorheight-10 and enemy.velY > 0 then -- reverse enemie speed if the move to the outside vertically
+				enemy.velY = -enemy.velY
+			end
 		end
-
 	end
 end
 
+function M.DrawEnemy(enemy)
+  love.graphics.setColor(255,0,0,255) -- some red for our enemies
 
+  for i, enemy in ipairs(objects.enemy) do  -- drawing some enemies
+    if enemy.isDone == 1 then
+      love.graphics.polygon("fill", enemy.body:getWorldPoints(enemy.shape:getPoints()))
+      love.graphics.print(tostring(enemy.velY),600, 10*i)
+      love.graphics.print(enemy.fixture:getUserData(),500, 10*i)
+      love.graphics.print(enemy.fixture:getUserData(),enemy.body:getX()+20, enemy.body:getY())
+    end
+  end
+end
 return M
